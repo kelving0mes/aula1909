bd94e8302b1f5024460668b26698d6b2a2a15370
kelving0mes <kelvingomes04@gmail.com>
2024-09-19 11:33:45 -0300

    aula 19-09

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TransactionProvider } from './src/context/TransactionContext';
+import { useContext } from 'react';
 
 export default function App() {
+  const {transaction} = useContext(TransactionContext)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TransactionProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TransactionProvider>
   );
 }
 
